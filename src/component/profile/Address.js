ab597a1f89c186cf186b536931506cc2d4084104
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-05-19 21:06:46 +0200

    Update Address.js

--- src/component/profile/Address.js
+++ src/component/profile/Address.js
@@ -1,31 +1,26 @@
 import React from "react";
 import Alert from 'react-bootstrap/Alert';
-const Address =()=>{
-return(
 
-<>
-
-<Alert variant="success">
-      <Alert.Heading>Address:</Alert.Heading>
-      <p>
-     
-      </p>
-      <hr />
-      <p className="mb-0">
-      R. 28 de Janeiro 350,
-4400-335 Vila Nova de Gaia
-Portugal
-      </p>
-    </Alert>
-
-
-
-
-
-</>
-
-)
-
-
+const Address = () => {
+  return (
+    <>
+      {/* Alert component with variant set to success */}
+      <Alert variant="success">
+        {/* Alert heading */}
+        <Alert.Heading>Address:</Alert.Heading>
+        <p>
+          {/* Empty paragraph */}
+        </p>
+        <hr />
+        <p className="mb-0">
+          {/* Address details */}
+          R. 28 de Janeiro 350,
+          4400-335 Vila Nova de Gaia
+          Portugal
+        </p>
+      </Alert>
+    </>
+  );
 }
-export default Address
+
+export default Address;
