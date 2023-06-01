dc2430453d2be591964991f3ffe46155200d5d46
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-06-01 20:48:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+{/* importation de notre styleSheet de boostrap */}
 import 'bootstrap/dist/css/bootstrap.min.css';
+{/* importation de nos composants */}
 import Profilephoto from './component/profile/Profilephoto';
 import Fullname from './component/profile/Fullname';
 import Address from './component/profile/Address';
+{/* fonction app  */}
 function App() {
   return (
    <>
+    {/* l'appelation de nos composants */}
    <Profilephoto/>
    <Fullname/>
  <Address/>
