f6bfdd212ea1a4c88121aaa3bbe2ccfa6ce71591
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-06-01 20:47:13 +0200

    Update Profilephoto.js

--- src/component/profile/Profilephoto.js
+++ src/component/profile/Profilephoto.js
@@ -1,7 +1,11 @@
 import React from 'react'
+{/* importation des composants de boostratp  */}
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+{/* importation d'une image  */}
+
 import i1  from '../../imagee/1.jpg'
+{/* fonction Profilephoto  */}
 const Profilephoto = () => {
   return (
     <div style={{ display : 'flex', justifyContent : 'center', marginTop : '50px'}}>
